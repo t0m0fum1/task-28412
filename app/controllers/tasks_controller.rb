b7bbcb0afb2fbd3b508b464692ed07a1a1a0c625
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-09-14 15:45:42 +0900

    active_hashのgemを導入、新規投稿とタスク一覧表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,28 @@
 class TasksController < ApplicationController
   def index
-    @task = Task.includes(:user)
+    @tasks = Task.includes(:user)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+
+  private
+  def task_params
+    params.require(:task).permit(:text, :priority_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :priority
+  belongs_to :user
+
+
+  validates :text, :priority, presence: true
+
+  validates :priority_id, numericality: { other_than: 1 }
+
 end
