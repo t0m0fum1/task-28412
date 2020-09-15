f0108afc3d39480c45dd1ab10afd6231f94264dc
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-09-15 19:47:40 +0900

    active_storage導入、コンテンツのルーティング、MVC準備、マイグレート済

---
+++ app/models/content.rb
@@ -0,0 +1,8 @@
+class Content < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :message, presence: true, length: {maximum: 40 }
+  validates :image, presence: true
+
+end
