102b61d05dc1f435a9dcebfdf2ad8e960d4522a0
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-09-23 20:25:45 +0900

    画像投稿機能に不具合があった為、修正

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  belongs_to :task
+  
 
   validates :message, presence: true, length: {maximum: 40 }
   validates :image, presence: true
